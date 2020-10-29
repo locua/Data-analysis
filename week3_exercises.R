9edc119f738a4c87c7c068eb00d6391492b26636
Louis <poes997@gmail.com>
2020-10-29 23:13:43 +0000

    latest

---
+++ week3_exercises.R
@@ -0,0 +1,30 @@
+library(car)
+library(ggplot2)
+
+dat <- read.csv("data/legacy2015.csv")
+
+# 1. Exploratory plots
+ggplot(dat, aes(x=age)) + geom_histogram(binwidth =1, col="black", fill="brown")
+ggplot(dat, aes(x=intention)) + geom_histogram(binwidth =1, col="black", fill="brown")
+ggplot(dat, aes(x=education)) + geom_histogram(binwidth =1, col="black", fill="brown")
+ggplot(dat, aes(x=income)) + geom_histogram(binwidth =1, col="black", fill="brown")
+ggplot(dat, aes(x=donation)) + geom_histogram(binwidth =1, col="black", fill="brown")
+ggplot(dat, aes(x=belief)) + geom_histogram(binwidth =1, col="black", fill="brown")
+
+
+ggplot(dat, aes(x=intention, y=age)) + geom_point()
+ggplot(dat, aes(x=education, y=age)) + geom_point()
+ggplot(dat, aes(x=income, y=donation)) + geom_point()
+ggplot(dat, aes(x=education, y=donation)) + geom_point()
+ggplot(dat, aes(x=education, y=legacy)) + geom_point()
+ggplot(dat, aes(x=education, y=intention)) + geom_point()
+ggplot(dat, aes(x=intention, y=belief)) + geom_point()
+ggplot(dat, aes(x=intention, y=legacy)) + geom_point()
+
+# 2. Predicting intention from legacy
+summary(lm(intention~1, data=dat))
+
+anova(lm(intention~1, data=dat), lm(intention~legacy, data=dat))
+# Reject the null hypothesis p < 0.000
+
+# 3. 
