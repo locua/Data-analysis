740e8fc93e920300f958e231c982170c7f368103
Louis <poes997@gmail.com>
2020-11-15 18:11:27 +0000

    ex6 w5

--- week5exercises.R
+++ week5exercises.R
@@ -12,6 +12,7 @@ anchoring$dummy <- ifelse(anchoring$anchor == "low", 0, 1)
 # Estimate regression model predicting everest_feet from dummy
 summary(lm(everest_feet~dummy, data=anchoring))
 # We have a significant result for the intercept and the dummy predictor
+
 # and therefore evidence that there is a difference between the mean
 # of the low and high anchored predictions of everest in feet.
 
@@ -20,7 +21,7 @@ anchoring$anchor <- as.factor(anchoring$anchor)
 # anchor high = 0.5
 # anchor low = -0.5
 #anchoring$effecta <- ifelse(anchoring$anchor=="high",0.5,-0.5)
-#summary(lm(everest_feet~effecta, data=anchoring))
+summary(lm(everest_feet~effecta, data=anchoring))
 # Using contrasts
 dat$anchor <- as.factor(dat$anchor)
 contrasts(dat$anchor)  
@@ -28,17 +29,16 @@ contrasts(dat$anchor)  <- c(.5,-.5)
 summary(lm(everest_feet~anchor, data=dat))
 
 # 4.
+contrasts(dat$anchor)  <- c(1,-1)
+summary(lm(everest_feet~anchor, data=dat))
 
+# 5.
+plot_raincloud(subset(anchoring, anchor == 'low' & referrer %in% c("tilburg","wisc","laurier")), everest_feet, groups=referrer)
 
-
-
-
-
-
-
-
-
-
-
-
-  
+# 6.
+dat1<-subset(anchoring, anchor == 'low' & referrer %in% c("tilburg","wisc","laurier"))
+codes <- cbind(c(-1/3,-1/3,2/3), c(-1/2,1/2,0))
+colnames(codes) <- c(" us-v-inter", " inter-only")
+dat1$referrer <- as.factor(dat1$referrer)
+contrasts(dat1$referrer) <- codes
+summary(lm(everest_feet ~ referrer, data=dat1))
